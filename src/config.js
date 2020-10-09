87999334c780021512ea899e7c13d6fe62c50c63
Peter <peter.m.nordman@gmail.com>
2020-10-09 09:39:45 -0700

    Triggering redeployment on Vercel

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
-    API_ENDPOINT: process.env.REACT_APP_ENV === "development"
-        ? `http://localhost:8000/api`
-        : process.env.REACT_APP_WEDO_SERVER,
-    API_KEY: process.env.REACT_APP_WEDO_API_KEY,
-}
+  API_ENDPOINT:
+    process.env.REACT_APP_ENV === 'development'
+      ? `http://localhost:8000/api`
+      : process.env.REACT_APP_WEDO_SERVER,
+  API_KEY: process.env.REACT_APP_WEDO_API_KEY,
+};
